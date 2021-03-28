92dca6188ae6cb556f71db2f1fe4f1fb06a7576e
kaiyangzhou <k.zhou.vision@gmail.com>
2021-03-28 10:51:07 +0800

    enable importing SimpleNet from dassl.engine.

--- dassl/engine/__init__.py
+++ dassl/engine/__init__.py
@@ -1,5 +1,5 @@
 from .build import TRAINER_REGISTRY, build_trainer # isort:skip
-from .trainer import TrainerX, TrainerXU, TrainerBase, SimpleTrainer # isort:skip
+from .trainer import TrainerX, TrainerXU, TrainerBase, SimpleTrainer, SimpleNet # isort:skip
 
 from .da import *
 from .dg import *
