2d4d78afac22320e0346040f913a870b5f0b3452
kaiyangzhou <k.zhou.vision@gmail.com>
2021-04-21 20:08:08 +0800

    fix typo

--- dassl/config/defaults.py
+++ dassl/config/defaults.py
@@ -49,7 +49,7 @@ _C.INPUT.COLORJITTER_C = 0.4
 _C.INPUT.COLORJITTER_S = 0.4
 _C.INPUT.COLORJITTER_H = 0.1
 # Random gray scale's probability
-_C.INPUT.INPUT.RGS_P = 0.2
+_C.INPUT.RGS_P = 0.2
 
 ###########################
 # Dataset
