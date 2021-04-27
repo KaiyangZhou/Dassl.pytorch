fdd8081bc75eed2cca70ae0a11f9bea840ec9955
kaiyangzhou <k.zhou.vision@gmail.com>
2021-04-27 12:07:15 +0800

    add SeqDomainSampler (samples K images from each domain)

--- dassl/data/samplers.py
+++ dassl/data/samplers.py
@@ -61,6 +61,59 @@ class RandomDomainSampler(Sampler):
         return self.length
 
 
+class SeqDomainSampler(Sampler):
+    """Sequential domain sampler, which randomly samples K
+    images from each domain to form a minibatch.
+
+    Args:
+        data_source (list): list of Datums.
+        batch_size (int): batch size.
+    """
+
+    def __init__(self, data_source, batch_size):
+        self.data_source = data_source
+
+        # Keep track of image indices for each domain
+        self.domain_dict = defaultdict(list)
+        for i, item in enumerate(data_source):
+            self.domain_dict[item.domain].append(i)
+        self.domains = list(self.domain_dict.keys())
+        self.domains.sort()
+
+        # Make sure each domain has equal number of images
+        n_domain = len(self.domains)
+        assert batch_size % n_domain == 0
+        self.n_img_per_domain = batch_size // n_domain
+
+        self.batch_size = batch_size
+        # n_domain denotes number of domains sampled in a minibatch
+        self.n_domain = n_domain
+        self.length = len(list(self.__iter__()))
+
+    def __iter__(self):
+        domain_dict = copy.deepcopy(self.domain_dict)
+        final_idxs = []
+        stop_sampling = False
+
+        while not stop_sampling:
+            for domain in self.domains:
+                idxs = domain_dict[domain]
+                selected_idxs = random.sample(idxs, self.n_img_per_domain)
+                final_idxs.extend(selected_idxs)
+
+                for idx in selected_idxs:
+                    domain_dict[domain].remove(idx)
+
+                remaining = len(domain_dict[domain])
+                if remaining < self.n_img_per_domain:
+                    stop_sampling = True
+
+        return iter(final_idxs)
+
+    def __len__(self):
+        return self.length
+
+
 class RandomClassSampler(Sampler):
     """Randomly samples N classes each with K instances to
     form a minibatch of size N*K.
@@ -141,6 +194,9 @@ def build_sampler(
 
     elif sampler_type == 'RandomDomainSampler':
         return RandomDomainSampler(data_source, batch_size, n_domain)
+
+    elif sampler_type == 'SeqDomainSampler':
+        return SeqDomainSampler(data_source, batch_size)
 
     elif sampler_type == 'RandomClassSampler':
         return RandomClassSampler(data_source, batch_size, n_ins)
