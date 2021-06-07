e79eadd1be8ea80db35a9929580f16ff961d189a
kaiyangzhou <k.zhou.vision@gmail.com>
2021-06-07 18:16:25 +0800

    compute pseudo labels' acc in fixmatch

--- dassl/engine/ssl/fixmatch.py
+++ dassl/engine/ssl/fixmatch.py
@@ -37,17 +37,37 @@ class FixMatch(TrainerXU):
         self.test_loader = self.dm.test_loader
         self.num_classes = self.dm.num_classes
 
+    def assess_y_pred_quality(self, y_pred, y_true, mask):
+        n_masked_correct = (y_pred.eq(y_true).float() * mask).sum()
+        acc_thre = n_masked_correct / (
+            mask.sum() + 1e-5
+        ) # accuracy after threshold
+        acc_raw = y_pred.eq(y_true).sum() / y_pred.numel() # raw accuracy
+        keep_rate = mask.sum() / mask.numel()
+        output = {
+            'acc_thre': acc_thre,
+            'acc_raw': acc_raw,
+            'keep_rate': keep_rate
+        }
+        return output
+
     def forward_backward(self, batch_x, batch_u):
         parsed_data = self.parse_batch_train(batch_x, batch_u)
-        input_x, input_x2, label_x, input_u, input_u2 = parsed_data
+        input_x, input_x2, label_x, input_u, input_u2, label_u = parsed_data
         input_u = torch.cat([input_x, input_u], 0)
         input_u2 = torch.cat([input_x2, input_u2], 0)
+        n_x = input_x.size(0)
 
-        # Generate artificial label
+        # Generate pseudo labels
         with torch.no_grad():
             output_u = F.softmax(self.model(input_u), 1)
-            max_prob, label_u = output_u.max(1)
+            max_prob, label_u_pred = output_u.max(1)
             mask_u = (max_prob >= self.conf_thre).float()
+
+            # Evaluate pseudo labels' accuracy
+            y_u_pred_stats = self.assess_y_pred_quality(
+                label_u_pred[n_x:], label_u, mask_u[n_x:]
+            )
 
         # Supervised loss
         output_x = self.model(input_x)
@@ -55,7 +75,7 @@ class FixMatch(TrainerXU):
 
         # Unsupervised loss
         output_u = self.model(input_u2)
-        loss_u = F.cross_entropy(output_u, label_u, reduction='none')
+        loss_u = F.cross_entropy(output_u, label_u_pred, reduction='none')
         loss_u = (loss_u * mask_u).mean()
 
         loss = loss_x + loss_u * self.weight_u
@@ -65,7 +85,9 @@ class FixMatch(TrainerXU):
             'loss_x': loss_x.item(),
             'acc_x': compute_accuracy(output_x, label_x)[0].item(),
             'loss_u': loss_u.item(),
-            'acc_u': compute_accuracy(output_u, label_u)[0].item()
+            'y_u_pred_acc_raw': y_u_pred_stats['acc_raw'],
+            'y_u_pred_acc_thre': y_u_pred_stats['acc_thre'],
+            'y_u_pred_keep': y_u_pred_stats['keep_rate']
         }
 
         if (self.batch_idx + 1) == self.num_batches:
@@ -79,11 +101,14 @@ class FixMatch(TrainerXU):
         label_x = batch_x['label']
         input_u = batch_u['img']
         input_u2 = batch_u['img2']
+        # label_u is used only for evaluating pseudo labels' accuracy
+        label_u = batch_u['label']
 
         input_x = input_x.to(self.device)
         input_x2 = input_x2.to(self.device)
         label_x = label_x.to(self.device)
         input_u = input_u.to(self.device)
         input_u2 = input_u2.to(self.device)
+        label_u = label_u.to(self.device)
 
-        return input_x, input_x2, label_x, input_u, input_u2
+        return input_x, input_x2, label_x, input_u, input_u2, label_u
