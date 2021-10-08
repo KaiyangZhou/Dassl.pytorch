edaef5a169fd4573c3abec89ce46a36ee74dda0c
kaiyangzhou <k.zhou.vision@gmail.com>
2021-10-08 16:18:43 +0800

    fix import error

--- dassl/engine/__init__.py
+++ dassl/engine/__init__.py
@@ -1,8 +1,6 @@
 from .build import TRAINER_REGISTRY, build_trainer  # isort:skip
+from .trainer import TrainerX, TrainerXU, TrainerBase, SimpleTrainer, SimpleNet  # isort:skip
+
 from .da import *
 from .dg import *
 from .ssl import *
-
-from .trainer import (  # isort:skip
-    TrainerX, SimpleNet, TrainerXU, TrainerBase, SimpleTrainer
-)
